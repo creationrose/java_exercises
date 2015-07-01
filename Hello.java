387968b123462949dd6d4997c7778de8db44304c
jessica <ladyjtm@gmail.com>
2015-07-01 18:06:52 +0900

    Scanner class test

--- Hello.java
+++ Hello.java
@@ -7,3 +7,24 @@ public class Hello {
   }
 
 }
+
+
+
+
+/*
+
+  import java.util.Scanner;
+     public class Main {
+
+      public static void main(String[] args) {
+
+      Scanner input = new Scanner(System.in);
+      System.out.print("What is your name? ");
+      String name = input.next();   next method in scanner class
+      System.out.println(String.format("Hello, %s", name));
+
+      }
+
+    }
+
+*/
