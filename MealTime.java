8be2bd556284b671d41ddc734c466489aeaf587d
jessica <ladyjtm@gmail.com>
2015-07-05 17:52:36 -0700

    DRY code for meal

--- MealTime.java
+++ MealTime.java
@@ -3,26 +3,20 @@ import java.io.Console;
 public class MealTime {
 
   public static void main(String[] args) {
-    Console myConsole = System.console();
 
     askForMeal("breakfast");
-    String breakfast = myConsole.readLine();
-    System.out.println("You had " + breakfast + " for breakfast.");
-
     askForMeal("lunch");
-    String lunch = myConsole.readLine();
-    System.out.println("You had " + lunch + " for lunch.");
-
     askForMeal("dinner");
-    String dinner = myConsole.readLine();
-    System.out.println("You had " + dinner + " for dinner.");
 
   }
 
-
   public static void askForMeal(String meal) {
 
+    Console myConsole = System.console();
+
     System.out.println("What did you eat for " + meal + "?");
+    String yourMeal = myConsole.readLine();
+    System.out.println("You had " + yourMeal + " for " + meal + ".");
 
   }
 
